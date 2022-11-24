0ca1e634bb0b59e45d67760b9c6e3287889577c2
Hori3538 <yu.m.h.94612@gmail.com>
2022-11-24 23:58:46 +0900

    made the box and the label appear . Fairly halfway through

--- dataset.py
+++ dataset.py
@@ -47,18 +47,27 @@ def get_transform(train: bool) -> T.Compose:
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
 
-# def draw_bbox(image: cv.Mat, rect: cv.Rect, int id)
+from torch.utils.data import DataLoader
+from argparse import ArgumentParser
+import cv2
+import numpy as np
+from typing import List
+
 
 def test() -> None:
-    from torch.utils.data import DataLoader
-    from argparse import ArgumentParser
-    import cv2
-    import numpy as np
-
     parser = ArgumentParser()
     parser.add_argument("--images-root-path", type=str)
     parser.add_argument("--json-annotation-path", type=str)
+    parser.add_argument("--label-file-path", type=str, default="./object_detection_classes_coco.txt")
+    parser.add_argument("--colors-file-path", type=str, default="./colors.txt")
     args = parser.parse_args()
+
+    class_names = np.loadtxt(args.label_file_path, dtype='str', delimiter='\n')
+    # print(class_names)
+    colors = np.loadtxt(args.colors_file_path, dtype='int', delimiter=' ')
+    # print(colors)
+    # print(len(colors[0]))
+    drawer = Drawer(class_names, colors)
 
     dataset = CustomDataset(root=args.images_root_path, annFile=args.json_annotation_path,
             transforms=get_transform(train=True))
@@ -73,13 +82,51 @@ def test() -> None:
 
         image = image[0].permute(1, 2, 0).cpu().numpy().astype(np.uint8)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        print(type(image))
-        print(image.shape)
-        # cv2.imshow("images", image)
-        # key = cv2.waitKey(0)
-        # if key == ord("q") or key == ord("c"):
-        #     break
-        # cv2.destroyAllWindows()
+        # print(f"shape of image: {image.shape}")
+
+        object_num = target[0]['labels'].nelement()
+        # print(f"object_num: {object_num}")
+        boxes = target[0]['boxes']
+        labels = target[0]['labels']
+        masks = target[0]['masks']
+        # print(f"type of mask: {type(masks)}")
+        # print(f"shape of mask: {masks.shape}")
+        for i in range(object_num):
+            # print(boxes[i].tolist())
+            rect: List[int] = [int(data) for data in boxes[i].tolist()]
+            id: int = labels[i]
+            mask = masks[i].numpy() * 255
+            # print(mask)
+            cv2.imshow("mask", mask)
+            key = cv2.waitKey(0)
+            if key == ord("q") or key == ord("c"):
+                break
+            cv2.destroyAllWindows()
+            # print(f"rect: {rect}")
+            drawer.draw_bbox(image, rect, id)
+
+        # print(type(image))
+        # print(image.shape)
+        cv2.imshow("images", image)
+        key = cv2.waitKey(0)
+        if key == ord("q") or key == ord("c"):
+            break
+        cv2.destroyAllWindows()
+
+class Drawer():
+    def __init__(self, class_names, colors) -> None:
+        self._class_names = class_names
+        self._colors = colors
+
+    def draw_bbox(self, image: np.ndarray, rect: List[int], id: int) -> None:
+        # print(f"rect[:2]: {rect[:2]}")
+        cv2.rectangle(image, rect[:2], rect[2:], (255, 255, 255), 2)
+        # cv2.rectangle(image, (50, 50), (50, 50), (255, 255, 255), 2)
+        cv2.putText(image, self._class_names[id-1], rect[:2],
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
+        
+
 
 if __name__ == "__main__":
     test()
